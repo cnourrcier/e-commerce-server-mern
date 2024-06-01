bfd7bd4dfd6872f6689100bcc217ca568b2e3442
Charlie Nourrcier <cnourrcier@hotmail.com>
2024-06-01 14:24:41 -0500

    Add rate limit to request password reset route

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -1,11 +1,11 @@
 const rateLimit = require('express-rate-limit');
 
-const resendVerificationLimiter = rateLimit({
+const rateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 5, // Limit each IP to 5 requests per window (15 minutes)
     message: {
-        message: 'Too many verification email requests from this IP. Please try again after 15 minutes'
+        message: 'Too many requests from this IP. Please try again after 15 minutes'
     }
 });
 
-module.exports = resendVerificationLimiter;
+module.exports = rateLimiter;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const { signup, login, requestPasswordReset, resetPassword, verifyEmail, resendVerificationEmail } = require('../controllers/authController');
 const { getUserProfile } = require('../controllers/userController');
 const protect = require('../middleware/authMiddleware');
-const resendVerificationLimiter = require('../middleware/rateLimiter');
+const rateLimiter = require('../middleware/rateLimiter');
 
 router.post('/signup', signup);
 router.post('/login', login);
-router.post('/request-password-reset', requestPasswordReset);
+router.post('/request-password-reset', rateLimiter, requestPasswordReset);
 router.put('/reset-password/:token', resetPassword);
 router.get('/verify-email/:token', verifyEmail);
-router.post('/resend-verification-email', resendVerificationLimiter, resendVerificationEmail);
+router.post('/resend-verification-email', rateLimiter, resendVerificationEmail);
 
 // Protected route to get user profile
 router.get('/profile', protect, getUserProfile);
