c1a8487746940e094087a48ffe93f3b271340627
Charlie Nourrcier <cnourrcier@hotmail.com>
2024-06-06 23:08:26 -0500

    troubleshoot deployment issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -102,7 +102,7 @@ exports.verifyEmail = async (req, res) => {
         user.verificationToken = undefined;
         await user.save();
 
-        res.redirect(`${process.env.FRONTEND_URL_PROD}/login`);
+        res.redirect(`/login`);
     } catch (err) {
         console.error('Error during email verification:', err); // Log the error for debugging
         res.status(500).json({
